5078a657edb16e2f52c7f4416e75e5eccb62a924
Raphaël Duhen <raphael.duhen@ign.fr>
2024-10-02 16:47:32 +0200

    feat: improve install script error handling

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,12 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+if [ "$(tput colors)" -gt 2 ]; then
+  RED="\033[1;31m"
+  RESET="\033[0m"
+else
+  RED=""
+  RESET=""
+fi
+
+function error() {
+  echo -e "${RED}Error: $1${RESET}"
+  echo "Please report this at \
+https://github.com/shellrhythm/shellrhythm-cli/issues if necessary."
+  exit 1
+}
 
 cd ~/.local/
+
 echo "Downloading shellrhythm to ~/.local/..."
-git clone https://github.com/HastagGuigui/shellrhythm.git
+git clone https://github.com/HastagGuigui/shellrhythm.git --depth=1
+[ $? -ne 0 ] && error "Failed to download shellrhythm from GitHub"
+
 cd ./shellrhythm
+
 echo "Installing required python dependencies... (there's like 3)"
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
+[ $? -ne 0 ] && error "Failed to download python dependencies"
+
 echo "Linking ~/.local/shellrhythm/shellrhythm to ~/.local/bin/shellrhythm..."
 ln -s ~/.local/shellrhythm/shellrhythm ~/.local/bin/shellrhythm
+[ $? -ne 0 ] && error "Failed to create symbolic link"
+
 echo "Successfully installed! You can now run the shellrhythm command to begin."
 echo "(If the command is not recognised, restart your terminal.)"
